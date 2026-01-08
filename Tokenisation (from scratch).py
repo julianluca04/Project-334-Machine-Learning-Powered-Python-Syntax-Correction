3f93ebc77b8d14a57d2608235fc05af4de121fa7
toni7lovric-rgb <toni7lovric@gmail.com>
2026-01-08 13:29:30 +0100

    .

--- Tokenisation (from scratch).py
+++ Tokenisation (from scratch).py
@@ -9,12 +9,15 @@ def vocabulary(df):
     full_text is a single string containing all characters from both columns
     we joined them by empty spaces and converted to string type (+ adds them together)
     we remove duplicates by converting string to set, then we convert it to list and sort it
+    Unkown is added in case we encounter multiple characters that are not in our vocabulary
+    Indent and dedent are represented by empty spaces (" ", "\n", "\r")
     """
     full_text = "".join(df["buggy_code"].astype(str)) + "".join(df["fixed_code"].astype(str))
     characters = sorted(list(set(full_text)))
 
     vocabulary_dictionary = {"<PAD>": 0, "<UNK>": 1, "<SOS>": 2, "<EOS>": 3}
     # this is a vocabulary dictionary, we immediately add special tokens with their IDs
+    # PAD = padding, UNK = unknown, SOS = start of sequence, EOS = end of sequence
 
     for char in characters: # we go through our list of characters
         if char not in vocabulary_dictionary: # if charaters is not already in dictionary
